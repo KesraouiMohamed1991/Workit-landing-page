7ba45ca079263139cad26ab69387ace57d8f9b61
kesraoui mohamed <kesraoui15003@gmail.com>
2023-08-23 16:09:10 +0200

    first commit --first version

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+
+  theme: {
+    color: {
+      violet: '#24053d'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
